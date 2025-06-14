5eb9b35e9f1d17910ab1c2454cc85d81584a8e22
aitzazisstuffed <aitzazimtiaz855@gmail.com>
2025-06-14 17:44:38 +0500

    reflect api changes

--- api/src/spirographicals/__init__.py
+++ api/src/spirographicals/__init__.py
@@ -1,19 +1,19 @@
-# spirographicals/__init__.py
+# api/src/spirographicals/__init__.py
 
 __version__ = "0.1.0"
 __author__ = "Aitzaz Imtiaz"
 
 try:
-    # Import the entire rust module as _internal
+    # This imports the functions and classes defined in the #[pymodule] in Rust.
     from . import spirographicals as _internal
 except ImportError as e:
     raise ImportError(
         "Could not import the compiled Rust extension for spirographicals. "
-        "Please build the project using 'make build' or 'make install-dev'. "
+        "Please run 'make build' or 'make install-dev'. "
         f"Original error: {e}"
     ) from e
 
-# Import the high-level Python API objects that users will interact with
+# Import the high-level Python API objects that users will interact with.
 from .objects import Figure, Axes
 from . import pyplot
 
@@ -23,5 +23,5 @@ __all__ = [
     'Axes',
     'pyplot',
     '__version__',
-    '__author__'
+    '__author__',
 ]
