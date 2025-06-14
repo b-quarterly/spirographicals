72a0b2d3060dc5ba9cc0420bac264f95fe12cd00
aitzazisstuffed <aitzazimtiaz855@gmail.com>
2025-06-14 17:33:51 +0500

    fix imports in `init` file properly

--- api/src/spirographicals/__init__.py
+++ api/src/spirographicals/__init__.py
@@ -1,36 +1,27 @@
 # spirographicals/__init__.py
-# Author: Aitzaz Imtiaz
-# Date: June 13, 2025
 
 __version__ = "0.1.0"
 __author__ = "Aitzaz Imtiaz"
 
-# --- Attempt to import the core Rust extension ---
 try:
-    # This imports the functions and classes defined in the `#[pymodule]` in Rust.
-    from .spirographicals import (
-        create_plot,
-        PlotConfig
-    )
+    # Import the entire rust module as _internal
+    from . import spirographicals as _internal
 except ImportError as e:
-    # Provide a helpful error message if the Rust extension isn't built.
     raise ImportError(
         "Could not import the compiled Rust extension for spirographicals. "
         "Please build the project using 'make build' or 'make install-dev'. "
         f"Original error: {e}"
     ) from e
 
-# --- Import the high-level Python API objects ---
+# Import the high-level Python API objects that users will interact with
 from .objects import Figure, Axes
 from . import pyplot
 
-# --- Define what gets imported with `from spirographicals import *` ---
+# Define what gets imported with `from spirographicals import *`
 __all__ = [
     'Figure',
     'Axes',
     'pyplot',
-    'create_plot',
-    'PlotConfig',
     '__version__',
     '__author__'
 ]
